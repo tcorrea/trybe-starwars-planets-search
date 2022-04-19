25cc1eab5948795f9d60de5c1aa003a12643ee03
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-04-19 18:36:31 -0300

    fix: lint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchField from '../components/SearchField';
 import Table from '../components/Table';
 
-const Home = (props) => (
+const Home = () => (
   <>
     <SearchField />
     <Table />
